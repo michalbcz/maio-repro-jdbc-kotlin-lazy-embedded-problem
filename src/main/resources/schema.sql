221f3e947071659006a7e4dda5dfa72bd9f03024
Marian Schubert <marian.schubert@gmail.com>
2024-06-21 17:25:21 +0200

    reproduce the problem

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE my_entity
+(
+    id   BIGINT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,15 +1,42 @@
 package com.example.demo
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Embedded
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+data class Money(
+    val amount: Int,
+    val currency: String,
+) {
+    val foo by lazy { 123 }
+}
+
+@Repository
+interface MyRepository : CrudRepository<MyRepository.MyEntity, Long> {
+    data class MyEntity(
+        @Id
+        val id: Long?,
+        val name: String,
+        @Embedded(prefix = "my_", onEmpty = Embedded.OnEmpty.USE_NULL)
+        val money: Money,
+    )
+}
 
 @Import(TestcontainersConfiguration::class)
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var myRepository: MyRepository
+
+    @Test
+    fun contextLoads() {
+        myRepository.deleteAll()
+    }
 
 }
